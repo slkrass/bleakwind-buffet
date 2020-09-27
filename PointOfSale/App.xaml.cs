d43e078ac0aea675386b2e128495af4b73722722
Stephanie Krass <slkrass@ksu.edu>
2020-09-27 15:48:04 -0500

    Added Pictures and Altered Buttons

--- PointOfSale/App.xaml.cs
+++ PointOfSale/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace PointOfSale
+namespace BleakwindBuffet.PointOfSale
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- PointOfSale/EntreeCustomizations/ThugsTBoneCustomization.xaml.cs
+++ PointOfSale/EntreeCustomizations/ThugsTBoneCustomization.xaml.cs
@@ -17,7 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PointOfSale
+namespace BleakwindBuffet.PointOfSale
 {
     /// <summary>
     /// Interaction logic for ThugsTBoneCustomization.xaml
